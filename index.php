3b0f2d8b7d5bb2294a1b67ca32543a5c9ac47e31
Amalia Litsa <amalia.litsa@wpengine.com>
2021-08-26 06:48:52 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Bricco Blends</title>
+<meta name="description" content="
+Bricco Blends is barista oat milk with 100% compostable packaging.
+">
+<meta name="keywords" content="oat milk, barista, eco friendly, compostable, latte, capuccino, foam, microfoam, local, austin">
+<meta name="author" content="Amalia Litsa
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css"
+      rel="stylesheet">
+<link rel="stylesheet" href="main.css">
+<base target="_blank" />
+
+<body>
+  <article>
+<section id="ecofriendly">
+<div class="row row-3">
+  <div class="col logo">
+    <img src="img/briccoblends.svg" alt="Bricco Blends" />
+    <h4>Coming Spring 2022</h4>
+    <p>
+      <strong>Bricco Blends</strong> is barista-designed oat milk for coffee shops. It produces gorgeous latte foam, and solves cost, waste, and storage problems caused by conventional milk cartons.
+    </p>
+    <p>
+      <a href="https://mailchi.mp/0322dcb68559/bricco-blends">Join our mailing list</a> to follow our progress to market launch.
+    </p>
+  </div>
+  <div class="col storage">
+    <h3><strong>6x</strong> storage space</h3>
+    <img src="img/6xstorage.svg" alt="Save storage space" />
+  </div>
+  <div class="col landfill">
+    <h3><strong>Zero</strong> landfill waste</h3>
+    <img src="img/zerowaste.svg" alt="Zero landfill waste" />
+  </div>
+</div>
+</section>
+<section id="freshmade">
+
+  <h2>
+    Set your coffee shop apart with
+    <span>house made, fresh &amp; local</span>
+  </h2>
+
+  <div class="row row-3">
+    <div class="col blend">
+      <h3><strong>1</strong>Blend</h3>
+      <figure><img src="img/blend.svg" alt="1. Blend" /></figure>
+      <p><strong>Make 3 gallons in just 7 minutes:</strong> Making fresh, house made oat milk doesn't require hours of pureeing and straining anymore. Bricco Blend does all the work&mdash;baristas just add water!</p>
+    </div>
+    <div class="col bottle">
+      <h3><strong>2</strong>Bottle</h3>
+      <figure><img src="img/bottle.svg" alt="2. Bottle" /></figure>
+      <p><strong>Use our label or your own:</strong> The final product is <em>your</em> bricco blend. Fresh made oat milk can be a new revenue stream for your shop.</p>
+    </div>
+    <div class="col basta">
+      <h3><strong>3</strong>Basta!</h3>
+      <figure><img src="img/basta.svg" alt="3. Basta!" /></figure>
+      <p><strong>Make art:</strong> Bricco Blends combines the perfect ratio of finely milled organic oats, sunflower oil, and minerals for a gorgeous, stable latte foam.</p>
+    </div>
+  </div>
+</section>
+</article>
+<footer>
+      <address>
+        <strong>Montopolis Beverages</strong><br />
+        720 Bastrop Hwy #303<br />
+        Austin, TX 78741
+      </address>
+      <p>
+      (512) 766 - 8255<br />
+      info@briccoblends.com
+      </p>
+      <p>
+        <a href="https://www.facebook.com/briccoblends">Facebook</a> &bull; <a href="https://www.instagram.com/briccoblends/">Instagram</a>
+      </p>
+</footer>
+
+</body>
+
+</html>
