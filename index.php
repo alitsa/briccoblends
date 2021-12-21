df8a0b25ad6c2d5c7f97d6b9172be32388f7df00
Amalia Litsa <walnut@Amalias-MacBook-Pro.local>
2021-12-21 11:26:20 -0600

    Dang. I'm so confused about FB pixel. Reverted to the previous one.

--- index.php
+++ index.php
@@ -43,22 +43,22 @@
  <meta name="facebook-domain-verification" content="9cjg4ic8g8tseuco90g0oa3y1aky3v" />
 
  <!-- Facebook Pixel Code -->
- <script>
-   !function(f,b,e,v,n,t,s)
-   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-   n.queue=[];t=b.createElement(e);t.async=!0;
-   t.src=v;s=b.getElementsByTagName(e)[0];
-   s.parentNode.insertBefore(t,s)}(window, document,'script',
-   'https://connect.facebook.net/en_US/fbevents.js');
-   fbq('init', '1249423722225144');
-   fbq('track', 'PageView');
- </script>
- <noscript><img height="1" width="1" style="display:none"
-   src="https://www.facebook.com/tr?id=1249423722225144&ev=PageView&noscript=1"
- /></noscript>
- <!-- End Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '871579672955387');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=871579672955387&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
 
 
 </head>
